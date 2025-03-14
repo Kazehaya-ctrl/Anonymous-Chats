9c10d0606043c89c825472c7f943de0c58250210
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-03-14 17:54:26 +0530

    upt: elysia backend

--- backend/schema/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-	name?: string;
-	email: string;
-}
-
-export interface Message {
-	id: string;
-	content: string;
-}
--- backend/schema/types.ts
+++
@@ -1,3 +0,0 @@
-import { ElysiaWS } from "elysia/dist/ws";
-
-export let wsClient: Map<string, ElysiaWS> = new Map();
